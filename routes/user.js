1bdf3d04cced7004c8ec3b88302379531818dabe
Palak Gupta <palakgupta2135@gmail.com>
2026-05-18 21:54:09 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,29 +3,24 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const userController = require("../controllers/user.js");
 
-// Middleware to save redirect URL
 const saveRedirectUrl = (req, res, next) => {
   if (req.query.redirect) {
     req.session.returnTo = req.query.redirect;
   }
   next();
 };
-const userController = require("../controllers/user.js");
 
-
-router.get("/signup",userController.rendersignupform );
-
+router.get("/signup", userController.rendersignupform);
 
 router.post(
   "/signup",
-  userController.signup
+  wrapAsync(userController.signup)
 );
 
-//  Login Form
 router.get("/login", userController.renderloginform);
 
-// Login Logic
 router.post(
   "/login",
   saveRedirectUrl,
@@ -37,7 +32,7 @@ router.post(
   },
   userController.login
 );
-// Logout
+
 router.get("/logout", userController.logout);
 
 module.exports = router;
