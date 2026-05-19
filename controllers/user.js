80cf5fd0f4910ed9dcef82859914ddad66dca30a
Akshara-codes <aksharajain030@gmail.com>
2026-05-19 13:00:28 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -9,9 +9,10 @@ module.exports.signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const newUser = new User({ username, email });
     const registeredUser = await User.register(newUser, password);
-
     req.login(registeredUser, (err) => {
-      if (err) return next(err);
+      if (err) {
+        return next(err);
+      }
       req.flash("success", "Welcome to Wanderlust!");
       res.redirect("/listings");
     });
