e0a276231260b99ca1aa44cca323731d5d6d2319
Akshara-codes <aksharajain030@gmail.com>
2026-05-19 13:01:15 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -21,17 +21,21 @@ router.post(
 
 router.get("/login", userController.renderloginform);
 
-router.post(
-  "/login",
-  saveRedirectUrl,
-  (req, res, next) => {
-    passport.authenticate("local", {
-      failureRedirect: "/login",
-      failureFlash: true,
-    })(req, res, next);
-  },
-  userController.login
-);
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email });
+    const registeredUser = await User.register(newUser, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+});
 
 router.get("/logout", userController.logout);
 
