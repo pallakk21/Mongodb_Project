e7d82749bb5e2423049c718825fbc46a8e177952
Akshara-codes <aksharajain030@gmail.com>
2026-05-19 13:10:11 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,23 +3,19 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
-const userController = require("../controllers/user.js");
 
+// Middleware to save redirect URL
 const saveRedirectUrl = (req, res, next) => {
   if (req.query.redirect) {
     req.session.returnTo = req.query.redirect;
   }
   next();
 };
+const userController = require("../controllers/user.js");
 
-router.get("/signup", userController.rendersignupform);
 
-router.post(
-  "/signup",
-  wrapAsync(userController.signup)
-);
+router.get("/signup",userController.rendersignupform );
 
-router.get("/login", userController.renderloginform);
 
 router.post("/signup", async (req, res, next) => {
   try {
@@ -37,6 +33,21 @@ router.post("/signup", async (req, res, next) => {
   }
 });
 
+//  Login Form
+router.get("/login", userController.renderloginform);
+
+// Login Logic
+router.post(
+  "/login",
+  saveRedirectUrl,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  userController.login
+);
+
+// Logout
 router.get("/logout", userController.logout);
 
 module.exports = router;
