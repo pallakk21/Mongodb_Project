f01666f57f73f70dce0fdd29727b91b0e774961a
Palak Gupta <palakgupta2135@gmail.com>
2026-05-20 12:01:43 +0530

    Refactor user registration and login logic

--- controllers/user.js
+++ controllers/user.js
@@ -10,9 +10,7 @@ module.exports.signup = async (req, res, next) => {
     const newUser = new User({ username, email });
     const registeredUser = await User.register(newUser, password);
     req.login(registeredUser, (err) => {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
       req.flash("success", "Welcome to Wanderlust!");
       res.redirect("/listings");
     });
@@ -28,8 +26,7 @@ module.exports.renderloginform = (req, res) => {
 
 module.exports.login = (req, res) => {
   req.flash("success", "Welcome back to Wanderlust!");
-  const redirectUrl = req.session.returnTo || "/listings";
-  delete req.session.returnTo;
+  const redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
 
