8d2d97c1c57c1f8f6bc9d4667fc8f7839223f207
Palak Gupta <palakgupta2135@gmail.com>
2026-05-20 12:01:10 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,42 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const userController = require("../controllers/user.js");
 
-// Middleware to save redirect URL
 const saveRedirectUrl = (req, res, next) => {
   if (req.query.redirect) {
     req.session.returnTo = req.query.redirect;
   }
   next();
 };
-const userController = require("../controllers/user.js");
 
+// Signup
+router.get("/signup", userController.rendersignupform);
+router.post("/signup", wrapAsync(userController.signup));
 
-router.get("/signup",userController.rendersignupform );
-
-
-router.post("/signup", async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-    const newUser = new User({ username, email });
-    const registeredUser = await User.register(newUser, password);
-    req.login(registeredUser, (err) => {
-      if (err) return next(err);
-      req.flash("success", "Welcome to Wanderlust!");
-      res.redirect("/listings");
-    });
-  } catch (e) {
-    req.flash("error", e.message);
-    res.redirect("/signup");
-  }
-});
-
-//  Login Form
+// Login
 router.get("/login", userController.renderloginform);
-
-// Login Logic
 router.post(
   "/login",
   saveRedirectUrl,
